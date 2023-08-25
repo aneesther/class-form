6e49331ca701af2f196d4a8ee6631f04cc8cc15a
esther <aneneesther40@gmail.com>
2023-08-25 17:58:23 +0100

    class-form

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("this is me greeting my console");console.log("hello")
+// let x = 2
+// let y = 6
+// let z = x+y;
+// console.log (z)
+let x=5, y=6;
+console.log(y)
+console.log ('John'+' Doe')
+function printMyName(firstname,lastname){
+    console.log(firstname+lastname)
+}
+console.log("Esther "+ "Ekene")
+let age=3
+let favNumber ="3"
